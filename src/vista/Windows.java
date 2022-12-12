3e3dd3209093a390aa9039deee380a7ce7750c50
ismaelor03 <xxismaelor03xx@gmail.com>
2022-12-12 18:49:47 +0100

    Creating windows Interface

--- src/vista/Windows.java
+++ src/vista/Windows.java
@@ -2,14 +2,22 @@ package vista;
 
 public interface Windows {
 
+    //Method that create the different buttons
     public void createButtons(int numButtons);
 
+    //Method that create the different panels
     public void createPanels(int numPanels);
 
+    //Method that create the different TextFields
     public void createTextFields(int numTextFields);
 
+    //Method that create the different Labels
     public void createLabels(int numLabels);
 
+    //Method that create the different ComboBox
     public void createJComboBox(int numComboBoxes);
+
+    //Method that set the different properties of the window
+    public void setDifferentProperties();
 
 }

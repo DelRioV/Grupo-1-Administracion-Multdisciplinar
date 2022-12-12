abeb50aa4614efd771741aca1be6cc84c3284494
ismaelor03 <xxismaelor03xx@gmail.com>
2022-12-12 18:45:08 +0100

    Creating windows Interface

---
+++ src/vista/Windows.java
@@ -0,0 +1,15 @@
+package vista;
+
+public interface Windows {
+
+    public void createButtons(int numButtons);
+
+    public void createPanels(int numPanels);
+
+    public void createTextFields(int numTextFields);
+
+    public void createLabels(int numLabels);
+
+    public void createJComboBox(int numComboBoxes);
+
+}

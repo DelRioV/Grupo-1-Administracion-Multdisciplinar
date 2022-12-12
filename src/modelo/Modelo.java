fd1c3f521ef420dc7d6824db0a8b47f90c076b74
ismaelor03 <xxismaelor03xx@gmail.com>
2022-12-12 19:41:03 +0100

    Login window implemented (not pretty)

--- src/modelo/Modelo.java
+++ src/modelo/Modelo.java
@@ -2,5 +2,40 @@ package modelo;
 
 public class Modelo {
 
+    private final static String LOGINWINDOWNAME = "Ventana de inicio de sesión";
+    private final static int LOGINWINDOWNUMBOTONES = 1;
+    private final static int LOGINWINDOWNUMPANELS = 1;
+    private final static int LOGINWINDOWNUMTEXTFIELDS = 2;
+    private final static int LOGINWINDOWNUMLABELS = 2;
+    private final static String [] LOGINWINDOWLABELSNAMES = {"Nombre de usuario","Contraseña"};
+    private final static String LOGINWINDOWBUTTONSAME = "Iniciar sesión";
+
+    private final static int LENGTHTEXTFIELDS = 20;
+
+    public int getLengthtextfields(){
+        return LENGTHTEXTFIELDS;
+    }
+
+    public String getLoginwindowname(){
+        return LOGINWINDOWNAME;
+    }
+    public int getLoginwindownumbotones(){
+        return LOGINWINDOWNUMBOTONES;
+    }
+    public int getLoginwindownumpanels(){
+        return LOGINWINDOWNUMPANELS;
+    }
+    public int getLoginwindownumtextfields(){
+        return LOGINWINDOWNUMTEXTFIELDS;
+    }
+    public int getLoginwindownumlabels(){
+        return LOGINWINDOWNUMLABELS;
+    }
+    public String [] getLoginwindowlabelsnames(){
+        return LOGINWINDOWLABELSNAMES;
+    }
+    public String getLoginwindowbuttonsame(){
+     return LOGINWINDOWBUTTONSAME;
+    }
 
 }

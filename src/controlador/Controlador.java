f7119f5241e070e6cef20c021ea3331be9edd3ad
angel <angel.acedo2003@gmail.com>
2022-12-16 08:23:34 +0100

    Download Multidisciplinar-DUAL.iml

--- src/controlador/Controlador.java
+++ src/controlador/Controlador.java
@@ -1,8 +1,14 @@
 package controlador;
 
+import modelo.ConnectionData;
+import org.apache.commons.net.ftp.FTPClient;
+
+import java.io.IOException;
+
 public class Controlador {
 
     public Controlador() {
+
     }
 
 }

d133e66bd5e92e27413bb3824a3290483e03a05b
ismaelor03 <xxismaelor03xx@gmail.com>
2022-12-16 11:03:17 +0100

    Upload Event succesfully

--- src/controlador/UploadEvent.java
+++ src/controlador/UploadEvent.java
@@ -6,22 +6,26 @@ import vista.UploadFilesAuxWindow;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 public class UploadEvent implements ActionListener {
 
-    private UploadFilesAuxWindow uploadFilesAuxWindow = new UploadFilesAuxWindow("hola");
+    private UploadFilesAuxWindow uploadFilesAuxWindow = null;
     private Modelo model = new Modelo();
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getActionCommand().equals("Subir fichero")){
+            uploadFilesAuxWindow=new UploadFilesAuxWindow("hola");
             addComponents();
         }
         else{
             try{
-                new UploadFiles().upload("admin","admin",uploadFilesAuxWindow.getTextFields().get(0).getText(),uploadFilesAuxWindow.getTextFields().get(1).getText());
+                File f = uploadFilesAuxWindow.getF().getSelectedFile();
+                new UploadFiles().upload("admin","admin",f.getAbsolutePath(),uploadFilesAuxWindow.getTextFields().get(1).getText());
                 uploadFilesAuxWindow.dispose();
                 JOptionPane.showConfirmDialog(null,"Fichero subido correctamente");
             }catch (Exception er){
+                uploadFilesAuxWindow.dispose();
                 JOptionPane.showConfirmDialog(null,"Los parámetros introducidos son incorrectos");
             }
 
@@ -34,8 +38,8 @@ public class UploadEvent implements ActionListener {
         uploadFilesAuxWindow.createLabels(model.getLoginwindownumlabels());
         uploadFilesAuxWindow.createPanels(model.getLoginwindownumpanels());
         uploadFilesAuxWindow.createTextFields(model.getLoginwindownumtextfields());
-        uploadFilesAuxWindow.getPanels().get(0).add(uploadFilesAuxWindow.getLabels().get(0));
-        uploadFilesAuxWindow.getPanels().get(0).add(uploadFilesAuxWindow.getTextFields().get(0));
+        uploadFilesAuxWindow.createJFileChooser();
+        uploadFilesAuxWindow.getPanels().get(0).add(uploadFilesAuxWindow.getF());
         uploadFilesAuxWindow.getPanels().get(0).add(uploadFilesAuxWindow.getLabels().get(1));
         uploadFilesAuxWindow.getPanels().get(0).add(uploadFilesAuxWindow.getTextFields().get(1));
         uploadFilesAuxWindow.getPanels().get(0).add(uploadFilesAuxWindow.getButtons().get(0));

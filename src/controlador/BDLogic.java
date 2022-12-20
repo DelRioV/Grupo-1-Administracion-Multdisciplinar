dabcb00776d1dca1a0bac490d48d2dfe41ceb202
ismaelor03 <xxismaelor03xx@gmail.com>
2022-12-20 18:04:46 +0100

    Create branch BdLogic

---
+++ src/controlador/BDLogic.java
@@ -0,0 +1,13 @@
+package controlador;
+
+public class BDLogic {
+
+    public boolean getAccess(String username,String password){
+        return false;
+    }
+
+    public void insertMove(String move){
+
+    }
+
+}

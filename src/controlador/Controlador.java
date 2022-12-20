cd7c85f8ccac07d6a948de17168a9c0da9497ff7
ismaelor03 <xxismaelor03xx@gmail.com>
2022-12-20 18:00:40 +0100

    Done test

--- src/controlador/Controlador.java
+++ src/controlador/Controlador.java
@@ -12,13 +12,7 @@ public class Controlador {
 
     private ConnectionDB myCon = new ConnectionDB();
     public Controlador() {
-        try {
-            ResultSet rs = myCon.getRemoteConnection().executeQuery("SELECT * FROM Users");
-            rs.next();
-            System.out.println(rs.getInt(1) + rs.getString(2)+rs.getString(3) + rs.getString(4));
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+
     }
 
 }
